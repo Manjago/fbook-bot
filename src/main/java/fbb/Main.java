4e4b16bef35bf278800c625de0b54cd4d3487b75
Kirill Temnenkov <temnenkov@yandex.ru>
2024-07-08 22:22:37 +0300

    shade plugin configured

--- src/main/java/fbb/Main.java
+++ src/main/java/fbb/Main.java
@@ -1,7 +1,15 @@
 package fbb;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final String json = new Gson().toJson(new Tost());
+        System.out.println(json);
+
+    }
+
+    private static class Tost {
+        private String hello = "world";
     }
 }
